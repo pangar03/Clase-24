4346b98460b9d357cb344d0b001e7d8919f3e632
Pangar03 <plopez0428@hotmail.com>
2024-04-18 15:32:47 -0500

    Add code, second commit

---
+++ script.js
@@ -0,0 +1,40 @@
+import {obtenerTareas} from "./utils.js";
+
+const render = async () => {
+    const tareas = await obtenerTareas();
+    const listaTareas = document.querySelector("#lista");
+
+    // Funcion para crear la tarea
+    const crearTarea = (nombreTarea, estadoTarea) => {
+        const tarea = document.createElement("div");
+        tarea.classList.add("lista__tarea");
+        tarea.textContent = nombreTarea;
+        if (estadoTarea) {
+            tarea.classList.add("lista__tarea--terminada");
+        }
+        else {
+            tarea.classList.add("lista__tarea--pendiente");
+        }
+
+        tarea.addEventListener("click", () => {
+            tarea.classList.toggle("lista__tarea--terminada");
+            tarea.classList.toggle("lista__tarea--pendiente");
+        });
+        
+        return tarea;
+    };
+
+    // Agregamos las tareas existentes al HTML
+    for(const tarea of tareas) {
+        listaTareas.appendChild(crearTarea(tarea.nombre, tarea.terminada));
+    }
+
+    // Evento para crear tarea
+    const formTareas = document.querySelector("#formulario");
+    formTareas.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        listaTareas.appendChild(crearTarea(e.target.nombreTarea.value, e.target.estadoTarea.checked));
+    });
+};
+
+document.addEventListener("DOMContentLoaded", render);
---
+++ utils.js
@@ -0,0 +1,7 @@
+export const obtenerTareas = async () => {
+    const response = await fetch(
+        "https://raw.githubusercontent.com/pangar03/Clase-24/desarrollo/data.json"
+    );
+    const data = await response.json();
+    return data;
+};
